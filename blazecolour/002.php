df40dc1fd9b44d9553a4cd12d8148c73114985bf
blazecolour <936x936@gmail.com>
2016-11-12 11:27:28 +0300

    #2 добавил функцию в задачю с 7 символами

--- blazecolour/002.php
+++ blazecolour/002.php
@@ -1,8 +1,14 @@
 <?php 
 $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam officiis sit ratione quibusdam commodi voluptatem tempore recusandae unde, exercitationem molestiae neque fugit, possimus inventore amet aperiam ullam laudantium in illo!';
 
+function correct_long_words ($text) {
+
 $arr = explode(" ", $text); //выводим текст в массив
 
 foreach ($arr as $value) {
 	echo mb_strimwidth("$value ", 0, 9, "* ");
 } //перебираем все слова, обрезая длинные до 7 символов, ставим * на место последнего символа в длинных словах
+
+}
+
+correct_long_words ($text);
