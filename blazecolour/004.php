51a6aef4ee7853cea10d26e07298cfa3ee25fce9
blazecolour <936x936@gmail.com>
2016-11-21 22:27:06 +0300

    #4 не дорешал задачу с городами

--- blazecolour/004.php
+++ blazecolour/004.php
@@ -1,17 +1,23 @@
 <?php
+$cities = [
+'Абакан', 'Абдулино', 'Агинское',
+'Барнаул', 'Белгород',
+'Великий Новгород', 'Владивосток',
+'Владикавказ', 'Владимир', 'Волгоград', 'Вологда', 'Воронеж',
+];
 
-$arr = array (
-'А' => array ('Абакан', 'Абдулино', 'Агинское'),
-'Б' => array ('Барнаул', 'Белгород',),
-'В' => array ('Великий Новгород', 'Владивосток', 'Владикавказ', 'Владимир', 'Волгоград', 'Вологда', 'Воронеж')
-);
+foreach ($cities as $city) {
+	$first_symb .=  reset(preg_split('//u', $city, null, PREG_SPLIT_NO_EMPTY));
+} // Выбираем все заглавные буквы
+
+$arr_first_symb = preg_split('//u', $first_symb, null, PREG_SPLIT_NO_EMPTY); // создаем массив с заглавными буквами
+
+$new_cities = array_combine($cities, $arr_first_symb); // создаем массив с ключами городами
 
 $dl = '';
-foreach ($arr as $key => $value1) {
-	$dl .= '<dt>' . $key . '</dt>';
-	foreach ($value1 as $value2) {
-		$dl .= '<dd>' . $value2 . '</dd>';
-	}
+foreach ($new_cities as $dd => $dt) {
+	$dl .= '<dt>' . $dt . '</dt>';
+	$dl .= '<dd>' . $dd . '</dd>';
 }
 $dl = '<dl>' . $dl . '</dl>';
 
