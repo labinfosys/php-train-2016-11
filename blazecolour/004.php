0e18b470346d9d560609d31747d9d551392638c0
blazecolour <936x936@gmail.com>
2016-12-17 17:04:44 +0300

    #4 решил задачу с городами

--- blazecolour/004.php
+++ blazecolour/004.php
@@ -6,19 +6,12 @@ $cities = [
 'Владикавказ', 'Владимир', 'Волгоград', 'Вологда', 'Воронеж',
 ];
 
+$firstLetter = '';
 foreach ($cities as $city) {
-	$first_symb .=  reset(preg_split('//u', $city, null, PREG_SPLIT_NO_EMPTY));
-} // Выбираем все заглавные буквы
-
-$arr_first_symb = preg_split('//u', $first_symb, null, PREG_SPLIT_NO_EMPTY); // создаем массив с заглавными буквами
-
-$new_cities = array_combine($cities, $arr_first_symb); // создаем массив с ключами городами
-
-$dl = '';
-foreach ($new_cities as $dd => $dt) {
-	$dl .= '<dt>' . $dt . '</dt>';
-	$dl .= '<dd>' . $dd . '</dd>';
+	if (mb_substr($city, 0, 1) != $firstLetter) {
+		$firstLetter = mb_substr($city, 0, 1);
+		$result .= '<dt>' . $firstLetter . '</dt>';
+	}	
+	$result .= '<dd>' . $city . '</dd>';
 }
-$dl = '<dl>' . $dl . '</dl>';
-
-echo $dl;
+echo '<dl>' . $result . '</dl>';
